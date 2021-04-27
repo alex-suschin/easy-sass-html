7817ddd37383a56f1a38c0fe6d5425093100359d
alex-suschin <alex_suschin@mail.ru>
2021-04-27 10:06:34 +0300

    mixin-scssClean

--- src/js/main.js
+++ src/js/main.js
@@ -16,10 +16,10 @@ $(function() {
         }
     });
 
-    jQuery("a.scrollto").click(function() {
-        elementClick = jQuery(this).attr("href")
-        destination = jQuery(elementClick).offset().top;
-        jQuery("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 700);
+    $("a.scrollto").click(function() {
+        elementClick = $(this).attr("href")
+        destination = $(elementClick).offset().top;
+        $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 700);
         return false;
     });
 
